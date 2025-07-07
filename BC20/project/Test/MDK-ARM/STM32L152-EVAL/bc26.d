b841fd3a51b278b3ecac62116155418ebe9cb520
zhuyoumianbing <164646147+zhuyoumianbing@users.noreply.github.com>
2025-07-07 16:27:48 +0800

    Add files via upload

---
+++ BC20/project/Test/MDK-ARM/STM32L152-EVAL/bc26.d
@@ -0,0 +1,1 @@
+.\stm32l152-eval\bc26.o: ..\..\HARDWARE\BC26\BC26.c
---
+++ BC20/project/Test/MDK-ARM/STM32L152-EVAL/hexstr.d
@@ -0,0 +1,3 @@
+.\stm32l152-eval\hexstr.o: ..\..\HARDWARE\STRHEX\HEXSTR.c
+.\stm32l152-eval\hexstr.o: ..\..\HARDWARE\STRHEX\HEXSTR.h
+.\stm32l152-eval\hexstr.o: D:\Keil_v5\ARM\ARMCC\bin\..\include\string.h
